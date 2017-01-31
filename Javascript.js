7e736ff428d9035bf48f9d62fcb91c90b4a6bfe3
Ryan Jowers <rjowers@g.clemson.edu>
2017-01-31 12:29:29 -0500

    adding event and made code more readable

--- Javascript.js
+++ Javascript.js
@@ -1,21 +1,16 @@
-
 var valueOne = document.getElementById("value-one")
 var valueTwo = document.getElementById("value-two");
 var button = document.getElementById("calculate-button");
-var Answer= document.getElementById("answer");
+var Answer = document.getElementById("answer");
 
 
-
-
-
-function Add ()
-{
-  var result= (Number(valueOne.value) + Number(valueTwo.value));
-  console.log(result);
-  Answer.innerHTML = result;
-  // 1. make it print out what's in text box 1 and text box 2
-  // 2. make it add those two things together and print it
-  // 3. make it put that on the page instead
-}
 
 button.addEventListener('click', Add);
+
+
+function Add(event) {
+    event.preventDefault();
+    var result = Number(valueOne.value) + Number(valueTwo.value);
+
+    Answer.innerHTML = result;
+}
